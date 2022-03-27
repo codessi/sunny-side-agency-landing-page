456525e482349fee0bfce774f659aba36a6e9a92
codessi <johansuh@gmail.com>
2022-03-26 18:33:32 -0700

    mvp

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from './../images/logo.svg'
 import hamburger from './../images/icon-hamburger.svg'
 
 const Nav = () => {
+  const [show, setShow] = useState(false)
+
+  const handleClick = () => {
+    setShow(!show)
+  }
+
   return (
     <nav>
       <img src={logo} alt="" />
-      <img src={hamburger} alt="" />
+      <ul className='desk-menu'>
+        <li>About</li>
+        <li>Serivce</li>
+        <li>Project</li>
+        <li>Contact</li>
+      </ul>
+      <img className='hamburger' onClick= {handleClick}src={hamburger} alt="" />
+      <ul className={show? 'mobile-menu': 'mobile-menu hide'}>
+        <li>About</li>
+        <li>Service</li>
+        <li>Project</li>
+        <li>Contact</li>
+      </ul>
+
     </nav>
   )
 }
